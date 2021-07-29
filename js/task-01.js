70dedfa6ace1c64d77dea8871ccd1bde4e10e15c
OLEG <schastliviy.sluchay@ukr.net>
2021-07-29 14:33:03 +0300

    Task 1.1

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const categoryCount = document.querySelectorAll('.item').length;
+// console.log('Всего категорий', categoryCount);
+
+const categoryFirstTitle = document.querySelector('h2').textContent;
+// --------------------------------------------------------------------
+
+const mark = document.querySelector('.item').childNodes[2];
+console.log(mark);
+
+
+
+// --------------------------------------------------------------------
+const categoryFirstCount = document.querySelectorAll('.item')
+// console.log(categoryFirstCount);
+// console.log(`Первая категория - название: '${categoryFirstTitle}', количество элементов: ${categoryFirstCount}`);
