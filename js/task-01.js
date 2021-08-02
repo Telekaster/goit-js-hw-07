eaf377bdaa5e0da3e1148bd117f907a4ee7e6d24
OLEG <schastliviy.sluchay@ukr.net>
2021-08-02 13:08:06 +0300

    task_1

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,20 @@
 const categoryCount = document.querySelectorAll('.item').length;
-// console.log('Всего категорий', categoryCount);
+console.log('Всего категорий', categoryCount);
 
-const categoryFirstTitle = document.querySelector('h2').textContent;
-// --------------------------------------------------------------------
+const CategoryArr = document.querySelectorAll('.item');
 
-const mark = document.querySelector('.item').childNodes[2];
-console.log(mark);
+const firstCategoryTitle = CategoryArr[0].firstElementChild.textContent;
+const firstCategoryCount = CategoryArr[0].children[1].children.length;
+console.log(`Категория: ${firstCategoryTitle}, Количество элементов: ${firstCategoryCount}`);
+
+const secondCategoryTitle = CategoryArr[1].firstElementChild.textContent;
+const secondCategoryCount = CategoryArr[1].firstElementChild.nextElementSibling.children.length;
+console.log(`Категория: ${secondCategoryTitle}, Количество элементов: ${secondCategoryCount}`);
 
 
+const thirdCategoryTitle = CategoryArr[2].firstElementChild.textContent;
+const thirdCategoryCount = CategoryArr[2].children[1].children.length;
 
-// --------------------------------------------------------------------
-const categoryFirstCount = document.querySelectorAll('.item')
-// console.log(categoryFirstCount);
-// console.log(`Первая категория - название: '${categoryFirstTitle}', количество элементов: ${categoryFirstCount}`);
+console.log(`Категория: ${thirdCategoryTitle}, Количество элементов: ${thirdCategoryCount}`);
+
+
