7acb60203e376a01618820078cea07a3bbc008d5
OLEG <schastliviy.sluchay@ukr.net>
2021-08-05 20:12:00 +0300

    task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,42 +1,76 @@
 // Забираем данные из input
-
 const input = document.querySelector('input')
-input.addEventListener('input', () => {
-    console.log(input.value);
-    return input.value;
-})
+const arr = [];
+let geometry = 20;
 
 
+function getValue() {
+console.log('Ввели:',input.value);
+    console.log('Преобразовали к числу:',Number(input.value));
 
+    for (let i = 1; i <= input.value; i+=1) {
+        arr.push(i)
+    }
+    console.log('Получили массив:', arr);
+    
+    
+        
 
-
-
-
-
+// ------------------------------------------
+// Создаём DIV
+    
+    arr.map(item => {
+        console.log('Создаём <div>');
+        createBoxes()
+    })    
+}
 
 function createBoxes(amount) {
+        
+    const createDiv = document.createElement('div');
+    createDiv.classList.add('action')
+   
+    // background-color
+    const randomRed = Math.round(Math.random() * (256 - 0) + 0);
+    const randomGreen = Math.round(Math.random() * (256 - 0) + 0);
+    const randomBlue = Math.round(Math.random() * (256 - 0) + 0);
+    console.log('Значение R', randomRed);
+    console.log('Значение G', randomGreen);
+    console.log('Значение B',randomBlue);
+    createDiv.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
 
-    const amountLength = [];
-    
-    // for (let i = 0; i < amount; i += 1){
-    //     amountLength.push(i)
-    // }
+    // height
+    arr.map(() => {
+        geometry += 10;
+        console.log('Геометрия', geometry);
+    })
+    createDiv.style.height = `${geometry}px`;
 
-    return amount;
-
-
-    // const createDiv = document.createElement('div');
-    // createDiv.style.width = '30px';
-    // createDiv.style.height = '30px';
-    // createDiv.style.backgroundColor = 'orange';
+    // width
+    createDiv.style.width = `${geometry}px`;
 
 
 
-
-    // const boxes = document.querySelector('#boxes')
-    // boxes.appendChild(createDiv);
+    // Вставляем DIV
+    const boxes = document.querySelector('#boxes')
+    boxes.appendChild(createDiv);
 
 };
 
-const createBtn = document.querySelector('[data-action="render"]')
-createBtn.addEventListener('click', createBoxes)
+// ------------------------------------------------
+
+// Кнопка "СОЗДАТЬ"
+const createBtn = document.querySelector('[data-action="render"]');
+createBtn.addEventListener('click', getValue)
+// ----------------------------------------------------
+
+// Кнопка "СТЕРЕТЬ"
+const removeBtn = document.querySelector('[data-action="destroy"]')
+function removeDiv() {
+    boxes.innerHTML = '';
+    input.value = '';
+}
+
+removeBtn.addEventListener('click', removeDiv)
+
+// --------------------------------------------
